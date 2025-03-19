ff733ed1ba1543ef2167884d72b0d0357a4b8924
nhi-batau <ssmemes163@gmail.com>
2025-03-19 20:17:25 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 27881923
-api_hash = "79abda5e46a51fc0dce1313f2548ce19"
-bot_token = "6263666911:AAF_Zm2baKNCJMGPn8aCBlVArvTa-A4kz1k"
+api_id = 20114039
+api_hash = "87297b8f3cc8fc9bbce591ad30da5896"
+bot_token = "7468678146:AAHiJc-Gp-T9ACMfwcBysTcpzlC9SY8Lg1I"
 
